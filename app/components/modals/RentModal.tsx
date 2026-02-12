7eee2b488d29bf5e430c6fd712dfe15da19451c8
UnicornDad8 <cecibenitezca@gmail.com>
2026-02-12 11:55:53 -0300

    Add description step to RentModal component

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
 import ImageUpload from '../inputs/ImageUpload';
+import Input from '../inputs/Input';
 
 enum STEPS {
   CATEGORY = 0,
@@ -24,6 +25,7 @@ enum STEPS {
 const RentModal = () => {
   const rentModal = useRentModal();
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const [isLoading, setIsLoading] = useState(false);
   const { register, handleSubmit, setValue, watch, formState: {errors}, reset } = useForm<FieldValues>({
     defaultValues: {
         category: "",
@@ -161,6 +163,34 @@ const RentModal = () => {
         <ImageUpload 
           value={imageSrc}
           onChange={(value) => setCustomValue("imageSrc", value)}
+        />
+      </div>
+    )
+  }
+
+  if(step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+          title="How would you describe your place?"
+          subtitle="Short and sweet works best!"
+        />
+        <Input 
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input 
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
         />
       </div>
     )
