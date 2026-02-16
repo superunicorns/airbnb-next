1f5507fb3b93fef87e4a940a06877a458c9b799f
UnicornDad8 <cecibenitezca@gmail.com>
2026-02-16 10:13:07 -0300

    Add listings to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-export default function Home() {
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+
+export default async function Home() {
+  const listings = await getListings();
+  
+  if(listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
+
   return (
-    <div className="text-2xl text-rose-500">
-      Hello Airbnb!
-    </div>
+    <ClientOnly>
+      <Container>
+        <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <div>
+            {listings.map((listing: any) => {
+              return (
+                <div>
+                  {listing.title}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
