efc0d6bf7bff9197650adfc1507765927b08989f
UnicornDad8 <cecibenitezca@gmail.com>
2026-02-17 09:35:10 -0300

    Finish HeartButton components with useFavorite hook

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { SafeUser } from "../types";
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import useFavorite from '../hooks/useFavorite';
 
 interface HeartButtonProps {
   listingId: string;
@@ -13,13 +14,12 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser
 }) => {
-  const hasFavorite = false;
-  const toggleFavorite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavorite({ listingId, currentUser });
 
   return (
     <div onClick={toggleFavorite} className="relative transition cursor-pointer hover:opacity-80">
       <AiOutlineHeart size={28} className="fill-white absolute -top-[2px] -right-[2px]" />
-      <AiFillHeart size={24} className={hasFavorite ? "fill-rose-500" : "fill-neutral-500/70"} />
+      <AiFillHeart size={24} className={hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"} />
     </div>
   )
 }
