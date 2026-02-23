25dfe7a030d74e997d9f34a29e5a42639e326b6a
UnicornDad8 <cecibenitezca@gmail.com>
2026-02-23 13:21:24 -0300

    Add Listing Id page heading

---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from 'react'
+import { SafeUser } from '@/app/types';
+import useCountries from '@/app/hooks/useCountries';
+import Heading from '../Heading';
+
+interface ListingHeadProps {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({
+  title,
+  locationValue,
+  imageSrc,
+  id,
+  currentUser
+}) => {
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading 
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+    </>
+  )
+}
+
+export default ListingHead;
