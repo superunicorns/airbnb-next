c7469132491d830061a536899c4b6bcf930f29cf
UnicornDad8 <cecibenitezca@gmail.com>
2026-02-25 11:06:43 -0300

    Finish ListingHead component

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import { SafeUser } from '@/app/types';
 import useCountries from '@/app/hooks/useCountries';
 import Heading from '../Heading';
+import Image from 'next/image';
+import HeartButton from '../HeartButton';
 
 interface ListingHeadProps {
   title: string;
@@ -30,6 +32,15 @@ const ListingHead: React.FC<ListingHeadProps> = ({
         title={title}
         subtitle={`${location?.region}, ${location?.label}`}
       />
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image fill src={imageSrc} alt="Image" className="object-cover w-full" />
+        <div className="absolute top-5 right-5">
+          <HeartButton 
+            listingId={id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
     </>
   )
 }
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -34,6 +34,9 @@ const ListingClient: React.FC<ListingClientProps> = ({
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className="grid grid-cols-1 mt-6 md:grid-cols-7 md:gap-10">
+
+          </div>
         </div>
       </div>
     </Container>
