7d23666f7b41dea48ba32b94174aa4e2b1c8aaad
UnicornDad8 <cecibenitezca@gmail.com>
2026-03-09 12:45:01 -0300

    Finish ReservationsClient reservations map ListingCard display

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -32,7 +32,9 @@ const RegisterModal = () => {
 
     axios.post("/api/register", data)
       .then(() => {
+        toast.success("User registered successfully");
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
         toast.error("Something went wrong!");
